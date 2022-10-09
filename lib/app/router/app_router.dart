45a6a6e713acfa4196ae915f95fe966fc05157a1
BenPrzychodzki <beniamin.przychodzki@gmail.com>
2022-10-09 16:09:56 +0200

    chore: update new feature template

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:gts_learn/presentation/feature/core/core_page.dart';
 import 'package:gts_learn/presentation/feature/dictionary/dictionary_page.dart';
 import 'package:gts_learn/presentation/feature/dictionary/wrapper/dictionary_wrapper_page.dart';
+import 'package:gts_learn/presentation/feature/home/home_page.dart';
+import 'package:gts_learn/presentation/feature/home/wrapper/home_wrapper_page.dart';
 import 'package:gts_learn/presentation/feature/lessons/lessons_page.dart';
 import 'package:gts_learn/presentation/feature/lessons/wrapper/lessons_wrapper_page.dart';
 
@@ -17,6 +19,14 @@ part 'app_router.gr.dart';
       path: '/',
       page: CorePage,
       children: [
+        AutoRoute(
+          path: 'lessons',
+          name: 'HomeRouter',
+          page: HomeWrapperPage,
+          children: [
+            AutoRoute(path: '', page: HomePage),
+          ],
+        ),
         AutoRoute(
           path: 'lessons',
           name: 'LessonsRouter',
