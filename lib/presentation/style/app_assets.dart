73a6f82edcf16da86fbb9132ebad079a9e8c38c2
BenPrzychodzki <beniamin.przychodzki@gmail.com>
2022-10-20 23:57:52 +0200

    chore: add home screen assets

--- lib/presentation/style/app_assets.dart
+++ lib/presentation/style/app_assets.dart
@@ -2,4 +2,8 @@ class AppAssets {
   AppAssets._();
   static const _imagePath = 'assets/images';
   static const logo = '$_imagePath/logo.png';
+  static const homeLessons = '$_imagePath/lessons_person.png';
+  static const homeDictionary = '$_imagePath/dictionary_person.png';
+  static const homeRanking = '$_imagePath/ranking_person.png';
+  static const background = '$_imagePath/background_dots.png';
 }
